82846758db9537d6efc8cb2be1091bf6c3657ca4
Rextouin-R <rattatouille1994@gmail.com>
2025-05-04 03:29:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -48,12 +48,12 @@ const displayWelcome = () => {
    .l0WMMMMMMWMMMNOl.    .cO┃ ┗━┛ ┃ ┃ ┏━┓ ┃ ┃ ┃ ┃ ┃ ┃ ┏━┓ ┏━┓ ┃ ┃ ┃ ┃ ┗━━┓ ┃ ┏━━┛ ┃ ━━ ┃${Colors.Teal}
      .;ok0KXK0Odl,.        l┗━━━ ━┛ ┗━┛ ┗━┛ ┗━┛ ┗━┛ ┗━┛ ┗━┛ ┗━┛ ┗━┛ ┗━━━━┛ ┗━━━━┛ ╰━━━━┛
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${Colors.Teal}{${Colors.Neon}Rextouin${Colors.Teal}}${Colors.RESET}
-\n${Colors.RESET}NODEGO Bot ${Colors.Blue}{ ${Colors.Neon}JS${Colors.Blue} }${Colors.RESET}
+\n${Colors.RESET}DAWN Validator ${Colors.Blue}{ ${Colors.Neon}JS${Colors.Blue} }${Colors.RESET}
 \n${Colors.Green}${'―'.repeat(60)}
 \n${Colors.Gold}[+]${Colors.RESET} JOIN : ${Colors.Teal}==> 🟦 join channel : https://t.me/UNLXairdop
 \n${Colors.Gold}[+]${Colors.RESET} FOLLOW : ${Colors.Teal}==> ⬛ github : https://github.com/Rextouin-R/
 \n${Colors.Green}${'―'.repeat(60)}
-\n${Colors.Gold}]-> ${Colors.Blue}{ ${Colors.RESET}NODEGO Extension${Colors.Neon} v1.1.4${Colors.Blue} } ${Colors.RESET}
+\n${Colors.Gold}]-> ${Colors.Blue}{ ${Colors.RESET}DAWN Extension${Colors.Neon} v1.1.4${Colors.Blue} } ${Colors.RESET}
 \n${Colors.Gold}]-> ${Colors.Blue}{ ${Colors.RESET}BOT${Colors.Neon} v1.0.0${Colors.Blue} } ${Colors.RESET}
 \n${Colors.Green}${'―'.repeat(60)}  
     `);
